bc61639dc1711c5e9762c438648bca7ce2c39aed
Alberto Herrera <angelus2112@gmail.com>
2025-03-15 10:47:07 -0600

    Soluciona el problema

--- app/app.py
+++ app/app.py
@@ -7,21 +7,22 @@ CATEGORIAS_VISITADAS = "D|N|X"
 
 def __obtener_datos(coleccion: Collection, desde: datetime):
     todos = coleccion.aggregate([
-        {"$match": {"fecha_transaccion": {"$g": desde}}}
+        {"$match": {"fecha_transaccion": {"$gt": desde}}}
     ])
     todos = list(todos)
     todos = pd.DataFrame(todos, columns=["nombre",
             "fecha_transaccion",
             "monto",
-            "recurrente"
+            "recurrente",
             "genero",
             "producto",
-            "categoria_favorita"
+            "categoria_favorita",
+            "categorias_mas_visitadas"
             ])
     return todos
 
 def __filtrar_datos(datos: pd.DataFrame):
-    datos = datos[datos['categorias_mas_visitada'].str.contains(CATEGORIAS_VISITADAS)]
+    datos = datos[datos['categorias_mas_visitadas'].str.contains(CATEGORIAS_VISITADAS)]
     return datos
 
 def __transformar_datos(datos: pd.DataFrame):
@@ -30,7 +31,7 @@ def __transformar_datos(datos: pd.DataFrame):
     return datos
 
 def __enviar(coleccion: Collection, datos: pd.DataFrame):
-    lista = ()
+    lista = []
     for i in datos.index:
         lista.append(datos.loc[i].to_dict())
     coleccion.insert_many(lista)
